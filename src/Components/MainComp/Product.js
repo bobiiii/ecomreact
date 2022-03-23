0e8e24602d36d97089809c0eb701eaf07b8aeebd
bobiiii <52751757+bobiiii@users.noreply.github.com>
2022-03-23 00:09:36 -0700

    edited product .js

--- src/Components/MainComp/Product.js
+++ src/Components/MainComp/Product.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import {fgfunc2} from "../Content/Data"
+import {fgfunc2,FragnanceData} from "../Content/Data"
 import {useParams} from "react-router-dom"
+import "./Product.css"
 const Product = () => {
     let param= useParams()
     let number = fgfunc2(parseInt(param.id,10))
-    console.log(param)
+    
     return (
-        <div>
-        <h1>i am full page component</h1>
-        {param.id}<br/>
-        {number.amount}<br/>
-        {number.desc}<br/>
-        {number.title}<br/>
+        <div className="product-main">
+           <div className="product-heading"><h2> {number.title}</h2></div>
+           
+        <div className="product-img"><img src={number.img}/></div>
+        <div className="product-details">
+            <h4 className="product-heading">Details</h4>
+        <div className="details-justify"><h5> Description</h5> <h5>{number.desc}</h5></div>
+        <div className="details-justify"><h5> Price</h5> <h5>{number.amount}</h5></div>
+        <div className="details-justify"><h5> Availibility</h5> <h5>{number.inStock}</h5></div>
+        
+        
+        </div>
         </div>    
     )
 }
